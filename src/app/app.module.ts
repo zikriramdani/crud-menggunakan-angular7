f6ae26d5eaa887d5dbadf8abd6d61b60f0d36ac2
Muhammad Riski <zikriramdani@gmail.com>
2020-04-25 18:11:55 +0700

    M

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { AngularFontAwesomeModule } from 'angular-font-awesome';
-// import icons from 'glyphicons';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatInputModule, MatFormFieldModule, MatTableModule, MatSortModule, MatPaginatorModule, MatDatepickerModule, MatNativeDateModule, } from '@angular/material';
+import 'hammerjs';
 
 // used to create fake backend
 import { fakeBackendProvider } from 'src/app/helpers';
 import { JwtInterceptor, ErrorInterceptor } from 'src/app/helpers';
 
 import { AppComponent } from 'src/app/app.component';
-import { HeaderComponent } from 'src/app/master/header/header.component';
-import { NavbarComponent } from 'src/app/master/navbar/navbar.component';
 import { LoginComponent } from 'src/app/login/login.component';
 import { RegisterComponent } from 'src/app/register/register.component';
 import { AlertComponent } from 'src/app/master/alert/alert.component';
+import { FooterComponent } from 'src/app/master/footer/footer.component';
 
 // Pages
 import { EmployeeComponent } from 'src/app/employee/employee.component';
@@ -27,11 +27,10 @@ import { AppRoutingModule } from 'src/app/app.routing.module';
 @NgModule({
     declarations: [
         AppComponent,
-        HeaderComponent,
-        NavbarComponent,
         LoginComponent,
         RegisterComponent,
         AlertComponent,
+        FooterComponent,
 
         // Pages
         EmployeeComponent,
@@ -39,19 +38,29 @@ import { AppRoutingModule } from 'src/app/app.routing.module';
     ],
     imports: [
         BrowserModule,
-        NgbModule,
         AppRoutingModule,
         ReactiveFormsModule,
         HttpClientModule,
-        // AngularFontAwesomeModule,
-        // icons
+        
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatFormFieldModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule
     ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
 
         // provider used to create fake backend
-        fakeBackendProvider
+        fakeBackendProvider,
+
+        MatDatepickerModule,
+        MatNativeDateModule
     ],
     bootstrap: [AppComponent]
 })
