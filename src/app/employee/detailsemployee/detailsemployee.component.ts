8da7bdbdc74118de063ff2d355cf01bc7f336eb6
Muhammad Riski <zikriramdani@gmail.com>
2020-04-26 16:27:33 +0700

    M

--- src/app/employee/detailsemployee/detailsemployee.component.ts
+++ src/app/employee/detailsemployee/detailsemployee.component.ts
@@ -20,27 +20,6 @@ export class DetailsEmployeeComponent implements OnInit {
     currentUserSubscription: Subscription;
     employees: Employee[] = [];
 
-    isLoading = true;
-    emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
-
-    myGroup = new FormControl();
-    options: string[] = [
-        'Group 1',
-        'Group 2',
-        'Group 3',
-        'Group 4',
-        'Group 5',
-        'Group 6',
-        'Group 7',
-        'Group 8',
-        'Group 9',
-        'Group 10'
-    ];
-    filteredOptions: Observable<string[]>;
-
-    minDate: Date;
-    maxDate: Date;
-
     constructor(
         private router: Router,
         private formBuilder: FormBuilder,
@@ -57,7 +36,7 @@ export class DetailsEmployeeComponent implements OnInit {
         this.employeeForm = this.formBuilder.group({
             fullName: ['', Validators.required],
             birthDate: ['', Validators.required],
-            email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+            email: ['', Validators.required],
             basicSalary: ['', Validators.required],
             myGroupControl: ['', Validators.required]
         });
